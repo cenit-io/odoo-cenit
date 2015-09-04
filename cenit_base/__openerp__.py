5e2584ae4a07be25c40fd5784103bdef10dba45c
D.H. Bahr <dhbahr@gmail.com>
2015-09-04 13:23:09 -0400

    Added dependencies to description

--- cenit_base/__openerp__.py
+++ cenit_base/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Cenit Integrations Client',
-    'version': '0.2.6',
+    'version': '0.2.7',
     'application': True,
     'author': 'Cenit, OpenJAF',
     'website': 'https://www.cenitsaas.com',
