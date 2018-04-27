3edf4d02fe7090ecc274966ad776145978fa1f3e
Alain Fernández Deroncere <alainfd82@gmail.com>
2018-04-27 15:35:28 -0400

    Modified cenit_base to operate with Mapping type translators. Modified cenit_base to Export and Import the Data types.

--- cenit_base/models/installer.py
+++ cenit_base/models/installer.py
@@ -424,7 +424,7 @@ class CollectionInstaller(models.TransientModel):
         names_pool = self.env['cenit.namespace']
 
         for translator in values:
-            if translator.get('_type') not in ('Setup::Parser', 'Setup::Renderer'):
+            if translator.get('_type') not in ('Setup::Parser', 'Setup::Renderer', 'Setup::MappingConverter'):
                 continue
             trans_data = {
                 'cenitID': translator.get('id'),
